66d1d140dce9160a66b32c140508ea4926bef933
YukiOzawa1527 <oz.eternal0127@gmail.com>
2025-02-15 04:46:35 +0000

    ビューの修正、エラーの解決

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource) 
+  def after_sign_in_path_for(resource)
+    case resource
+  when Admin
+    admin_root_path
+  when User
     my_page_path
+  end
   end
 end
